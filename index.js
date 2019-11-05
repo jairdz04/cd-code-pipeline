2a463055addf5c94297adb176e0519c49dfb62a3
jairdz04 <jairdz04@gmail.com>
2019-11-05 16:47:36 -0500

    api test

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/test', (req, res) => res.send({ message: 'connected' }));
+
+app.listen(7000, () => {
+  console.log('API IS RUNNING ON PORT 3000');
+});
