887894a3feddb021e074b15b0ba2a44af4a624f6
jairdz04 <jairdz04@gmail.com>
2019-11-06 08:06:05 -0500

    changing port

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('/test', (req, res) => res.send({ message: 'connected' }));
 
-app.listen(7000, () => {
+app.listen(3000, () => {
   console.log('API IS RUNNING ON PORT 3000');
 });
