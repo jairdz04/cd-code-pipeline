64ef72c915853ce205c5eef26e4128a18ea38f31
jairdz04 <jairdz04@gmail.com>
2019-11-15 16:00:35 -0500

    addin stringify

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/test', (req, res) => res.send({ message: 'connected' }));
+app.get("/test", (req, res) => res.send({ message: "connected" }));
 
-app.listen(3000, () => {
-  console.log('API IS RUNNING ON PORT 3000');
+app.post("/test", (req, res) => {
+  console.log(JSON.stringify(req.body));
+  res.send({ message: "connected" });
+});
+
+app.listen(7000, () => {
+  console.log("API IS RUNNING ON PORT 7000");
 });
